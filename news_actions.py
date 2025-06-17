68e736ed17be402df22392b15018c927e26b3a0c
longlodw <longlodw@gmail.com>
2025-06-17 19:16:51 -0400

    change default topic

--- news_actions.py
+++ news_actions.py
@@ -63,7 +63,7 @@ def ingest_news(
         old_messages_contents.append(f"what is the most probable topic of interest to {User.ASKER} based on recent chat messages in 1 phrase?")
         interests = generate_text(old_messages_contents)
     else:
-        interests = ""
+        interests = "breaking news"
     latest_news = get_latest_news(from_time, interests)
     if len(latest_news) == 0:
         return "No articles found for the specified time and interests."
